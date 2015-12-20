db063a67d883faa6d7f20f49d6e4db8ee471475d
Suzuki Hiroki <suzuki.hiroki@eiwa.com>
2015-12-20 22:14:49 +0900

    Java演習問題001完了しました。

---
+++ practice_h_suzuki/src/Test03.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2015 EIS Co., Ltd. All rights reserved.
+ */
+
+/**
+ * @author （作成者） <br />
+ *  （クラス論理名） <br />
+ *  （説明） <br />
+ *  更新履歴 yyyy/mm/dd （更新者）：（説明） <br />
+ */
+public class Test03 {
+
+  /**
+   * （メソッド論理名） <br />
+   * （説明） <br />
+   * @param args
+   */
+  public static void main( String[] args  ) {
+    
+    System.out.println("Javaの演習問題です。");
+    System.out.println("eclipseで色々と試行錯誤しながら進めてみましょう。");
+    System.out.println("よろしくお願いします。");
+  }
+
+}
